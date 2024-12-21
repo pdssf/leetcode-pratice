89da119ba6d88b92ddd7d6c6fee769f7d46a52d7
Paulo Salgado Filho <paulodiogenes2012@gmail.com>
2024-12-21 14:40:20 -0300

    fix: fixing canFinish and adding a new version using in-degree

--- src/graph/207.courseSchedule.js
+++ src/graph/207.courseSchedule.js
@@ -3,39 +3,70 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-var canFinish = function(numCourses, prerequisites) {
-    let deps = new Array(numCourses)
+var canFinish2 = function(numCourses, prerequisites) {
+    // create graph with adjacency list
+    let deps = Array.from({length: numCourses}, () => new Set())
     let done = []
-    for(let e of deps){
-        e = new Set()
-    }
 
+    // mount graph
     for(let e of prerequisites){
         deps[e[0]].add(e[1])
     }
 
     let noDeps = []
 
-    for(let e of deps){
+    // create list of nodes with no incoming edges 
+    deps.forEach((e, i) => {
         if(e.size == 0){
-            noDeps.push(i)
             done.push(i)
+            noDeps.push(i)
         }
-    }
+    })
 
+    // visit the graph (array done) and put the resulting node in the list to be visited
+    // if there is no other adjacent 
     while(noDeps.length > 0){
-        let matter = noDeps.pop()
-        for(let e of deps){
-            if(e.delete(matter) && e.size == 0){
-                let i = deps.indexOf(e)
-                noDeps.push(i)
-                done.push(i)
+        let course = noDeps.pop()
+        for(let i=0; i<deps.length; i++){
+            if(deps[i].has(course)){
+                deps[i].delete(course)
+                if(e.size == 0){
+                    noDeps.push(i)
+                    done.push(i)
+                }
             }
         }
-        if(done.length == numCourses){
-            return true
-        }
     }
-    return false
+    return done.length == numCourses
 };
 
+
+function canFinish (numCourses, prerequisites) {
+    // approach  using in-degree
+    let inDegree = Array.from({length: numCourses})
+    let adjacency = Array.from({length: numCourses}, () => [])
+    let noDeps = []
+
+    for(let [u,v] of prerequisites){
+        adjacency[v].push(u)
+        inDegree[u]++
+    }
+
+    inDegree.forEach((e,i) => {
+        if(e == 0){
+            noDeps.push(i)
+        }
+    })
+    let coursesTaken = 0
+    while(noDeps.length > 0) {
+        let course = noDeps.pop()
+        coursesTaken++
+        for(let node of adjacency[course]){
+            inDegree[node]--
+            if(inDegree[node] == 0) {
+                noDeps.push(node)
+            }
+        }
+    }
+    return coursesTaken == numCourses
+}
